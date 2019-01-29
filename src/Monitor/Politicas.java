7c5c30037fe6437cb6f720b490f14f4cb8d1bbb5
nick22xT <nicof_ramirez95@hotmail.com>
2019-01-29 17:40:44 -0300

    se agregan recursos y politica

--- src/Monitor/Politicas.java
+++ src/Monitor/Politicas.java
@@ -3,23 +3,23 @@ package Monitor;
 public class Politicas {
 	@SuppressWarnings("unchecked")
 	
-	private int[][] m_prioridad = new int[][] 	{{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	private int[][] m_prioridad = new int[][] { {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, //T0
 												{0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-												{0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+												{0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, // T2
 												{0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-												{0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+												{0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0}, // T4
 												{0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
-												{0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+												{0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0}, // T6
 												{0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
-												{0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
+												{0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0}, // T8
 												{0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
-												{0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+												{0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0}, // T10
 												{0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0},
-												{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0},
+												{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0}, // T12
 												{0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0},
-												{0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0},
+												{0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0}, // T14
 												{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0},
-												{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0},
+												{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0}, // T16
 												{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}};
 
 	
@@ -33,15 +33,6 @@ public class Politicas {
 		int[] m_int=parseInt(m);
 		int[] res_prod=productMatriz(m_int);
 		
-		/*for(int i = 0; i < m.length; i++) {
-			
-			if(m[i]) {
-				
-				cola = colas[i];
-				break;
-			}
-		}*/
-		
 		for(int i=0; i<res_prod.length; i++)
 		{
 			if(res_prod[i]==1)
@@ -50,25 +41,20 @@ public class Politicas {
 				break;
 			}
 		}
-		
 		return cola;
 	}
 	
 	public int[][] getM_prioridad() {
-		
 		return m_prioridad;
 	}
 	
 	
 	public int[] parseInt(boolean[] m) {
-		
 		int[] aux = new int[m_prioridad.length];
 		
 		for(int i = 0; i < m.length; i++) {
-			
 			aux[i] = m[i] ? 1 : 0;
 		}
-		
 		return aux;
 	}
 	
@@ -78,17 +64,13 @@ public class Politicas {
 	 * @return un vector de una dimension
 	 */
 	public int[] productMatriz(int[] vect) {
-		
 		int[] mult = new int[m_prioridad.length];
 		
 		for(int i = 0; i < m_prioridad.length; i++) {
-			
 			for(int j = 0; j < m_prioridad.length; j++) {
-				
 				mult[i] += m_prioridad[i][j]*vect[j];
 			}
 		}
-		
 		return mult;
 	}
 
